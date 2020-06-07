a0264f94d605e6cae3e4cb85fa67544f3da117a0
Vlad <vladsach@gmail.com>
2020-06-07 17:18:34 +0300

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+<<<<<<< Updated upstream
 from new_coords import read_txt
 
 info = read_txt('mercator_coords.txt')
+=======
+while True:
+    try:
+        x, y, type, undertype, name, adress = input().split(";")
+    except ValueError:
+        print('Неверное кол-во аргументов')
+    finally:
+        if adress[1] == ';':
+            adress = adress[0]
+        break
+
+print(x, y, type, undertype, name, adress)
+>>>>>>> Stashed changes

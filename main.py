f7570d1e44b787ce1616183acfcc5afb5d758d3a
Vlad <vladsach@gmail.com>
2020-06-07 17:22:53 +0300

    changed input

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-<<<<<<< Updated upstream
 from new_coords import read_txt
 
 info = read_txt('mercator_coords.txt')
-=======
+
+
 while True:
     try:
-        x, y, type, undertype, name, adress = input().split(";")
+        x, y, type, undertype, name, adress, *unused = input().split("; ")
+        del unused
     except ValueError:
         print('Неверное кол-во аргументов')
     finally:
-        if adress[1] == ';':
-            adress = adress[0]
         break
 
-print(x, y, type, undertype, name, adress)
->>>>>>> Stashed changes
+print(adress)
